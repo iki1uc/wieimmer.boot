4aaebd331b7f98e0f19a075212438e36d2789efc
iki1uc <entertrainer@web.de>
2026-09-02 04:51:31 +0200

    Create 9.js

---
+++ js/9.js
@@ -0,0 +1,6 @@
+Array.from({length:9}, ...)
+qi = ((r*3 + c*7 + 1) % 9) + 1
+iqq = ((r*5 + c*2 + 3) % 9) + 1
+echo = (ghost + in + out) % 9
+echo = (cell.ghost + cell.in + cell.out) % 9
+echo = (pump + cpuJumpC) % 9
